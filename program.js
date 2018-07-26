cba272f7e69915bc394ac7ba377a923565b3b626
Kat Lasota <lasota.kat@gmail.com>
2018-07-26 21:13:02 +0200

    first commit

---
+++ program.js
@@ -0,0 +1,24 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if(input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case 'node':
+                process.stdout.write(+process.versions.node+'\n');
+                break;
+            case 'lang':
+                process.stdout.write(+process.env.LANG+'\n');
+                break;
+            default: 
+                process.stderr.write('Wrong instruction!\n');
+
+        }
+        
+       
+    }
+});
